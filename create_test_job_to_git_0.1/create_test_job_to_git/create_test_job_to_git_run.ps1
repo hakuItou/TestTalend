f0de87e493a41fe9ca84358726a1daeb1757183b
hakuItou <51108012+hakuItou@users.noreply.github.com>
2019-06-17 15:53:38 +0900

    Add files via upload

---
+++ create_test_job_to_git_0.1/create_test_job_to_git/create_test_job_to_git_run.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/commons-collections4-4.1.jar;../lib/dom4j-1.6.1.jar;../lib/geronimo-stax-api_1.0_spec-1.0.1.jar;../lib/log4j-1.2.17.jar;../lib/poi-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-schemas-3.16-20170419.jar;../lib/poi-scratchpad-3.16-20170419.jar;../lib/talendcsv.jar;../lib/xmlbeans-2.6.0.jar;create_test_job_to_git_0_1.jar;' local_project.create_test_job_to_git_0_1.create_test_job_to_git  --context=Default %*
