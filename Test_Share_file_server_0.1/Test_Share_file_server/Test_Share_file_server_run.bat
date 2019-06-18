324e4c852e3e88e0ae5eb7f77ae5151351b4ca88
hakuItou <51108012+hakuItou@users.noreply.github.com>
2019-06-18 15:21:04 +0900

    Add files via upload

---
+++ Test_Share_file_server_0.1/Test_Share_file_server/Test_Share_file_server_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/commons-collections4-4.1.jar;../lib/dom4j-1.6.1.jar;../lib/geronimo-stax-api_1.0_spec-1.0.1.jar;../lib/log4j-1.2.17.jar;../lib/poi-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-schemas-3.16-20170419.jar;../lib/poi-scratchpad-3.16-20170419.jar;../lib/talendcsv.jar;../lib/xmlbeans-2.6.0.jar;test_share_file_server_0_1.jar; local_project.test_share_file_server_0_1.Test_Share_file_server  --context=Default %*
